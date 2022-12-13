480853b7a1d2917e4f5410dfeb1e15edb3ebb313
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-12-14 00:49:11 +0600

    .

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,26 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "./../components/Footer/Footer";
 import "./main.css";
 
 const Main = () => {
-    
     return (
-        <>
-            
-            <div className=" page-container ">
-                <div  className="z-50 duration-300  sticky top-1">
-                    <Navbar></Navbar>
-                </div>
-                <div id="home" className=" pb-40 -z-10">
-                    <Outlet></Outlet>
-                </div>
-                <div className="footer mb-6" >
-                    <Footer></Footer>
-                </div>
+        <div className=" page-container ">
+            <div className="z-50 duration-300  sticky top-1">
+                <Navbar></Navbar>
             </div>
-        </>
+            <div id="home" className=" pb-40 -z-10">
+                <Outlet></Outlet>
+            </div>
+            <div className="footer mb-6">
+                <Footer></Footer>
+            </div>
+        </div>
     );
 };
 
