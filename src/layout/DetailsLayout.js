1012cb46218a6c5ff6170b6fd3ef499318ce54df
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-12-20 20:25:47 +0600

    LOL

--- src/layout/DetailsLayout.js
+++ src/layout/DetailsLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import DetailsNavbar from "../components/Navbar/DetailsNavbar";
-import Navbar from "../components/Navbar/Navbar";
+// import Navbar from "../components/Navbar/Navbar";
 import Footer from "./../components/Footer/Footer";
 import ScrollToTop from './../assets/ScrollToTop';
 import "./main.css";
